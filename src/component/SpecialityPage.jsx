b743e675173cc8b7d792e023df4e5561415c3f2a
vikraman1653763 <vikraman1653763@gmail.com>
2025-02-14 10:12:02 +0530

    14-02-2025

--- src/component/SpecialityPage.jsx
+++ src/component/SpecialityPage.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import '../style/special-template.css';
 import { FaUserDoctor } from "react-icons/fa6";
+
 const SpecialityPage = ({ data }) => {
   const [validData, setValidData] = useState(null);
+
+  useEffect(() => {
+    AOS.init({ duration: 500 }); // Initialize AOS with a duration of 500ms
+  }, []);
 
   useEffect(() => {
     if (data) {
@@ -17,42 +24,37 @@ const SpecialityPage = ({ data }) => {
       {validData ? (
         <div className="special-page">
           {/* Title */}
-          <h1 className="special-title">{validData.title}</h1>
+          <h1 className="special-title" >{validData.title}</h1>
 
           {/* First Section */}
-          <div className="special-section">
-            <div className="special-text">{validData.description1}</div>
+          <div className="special-section" >
+            <div className="special-text" data-aos="fade-right">{validData.description1}</div>
             <div className="special-image">
               <img src={validData.image} alt={validData.title} />
             </div>
           </div>
 
           {/* Second Section */}
-          <div className="special-section">
+          <div className="special-section" data-aos="fade-up">
             <div className="special-logo">
               <img src={validData.logo} alt={validData.title} />
             </div>
-            <div className="special-text-2">{validData.description2}</div>
+            <div className="special-text-2" data-aos="fade-left">{validData.description2}</div>
           </div>
 
           {/* Third Section */}
-          <div className="special-section">
+          <div className="special-section" data-aos="fade-up">
             <div className="special-text">{validData.description3}</div>
           </div>
 
           {/* Our Doctors Section (Only if doctors are available) */}
           {validData.doctors && validData.doctors.length > 0 && (
-            <div className="doctors-section">
+            <div className="doctors-section" >
               <h3 className="doctors-title">Our Doctors</h3>
               <div className="doctors-list">
                 {validData.doctors.map((doctor, index) => (
-                  <div className="doctors-card" key={index}>
+                  <div className="doctors-card" key={index} data-aos="fade-up">
                     <div className="doctors-img-container">
-                      {/* <img
-                        className="doctors-img"
-                        src="/assets/user3.webp"
-                        alt={doctor.doctor_name}
-                      /> */}
                       <FaUserDoctor className='doctors-img'/>
                     </div>
                     <div className="doctor-info">
@@ -67,7 +69,7 @@ const SpecialityPage = ({ data }) => {
           )}
         </div>
       ) : (
-        <p>No data available to display.</p>
+        <p data-aos="fade-up">No data available to display.</p>
       )}
     </div>
   );
