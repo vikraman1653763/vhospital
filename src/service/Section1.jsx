1627c728f59fec042f11ecff7f3f7a7c33945d77
vikraman1653763 <vikraman1653763@gmail.com>
2025-02-18 14:32:02 +0530

    18-02-2025

---
+++ src/service/Section1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Section1 = () => {
+  return (
+    <section className="service-sec-1">
+      <div className="service-sec-1-banner">
+        <div className="service-sec-1-content">
+          <h1 className="service-sec-1-title">Comprehensive Healthcare Services</h1>
+          <p className="service-sec-1-description">
+            Our hospital offers a wide range of medical services, from emergency care to specialized treatments, ensuring the best possible care for all patients. Whether it's trauma, health checkups, or rehabilitation, we are equipped to handle all your healthcare needs with the utmost professionalism and care.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+export default Section1;
