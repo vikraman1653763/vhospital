2952f00395b4588344504b7c296c20f4c8450815
vikraman1653763 <133756180+vikraman1653763@users.noreply.github.com>
2025-03-04 23:43:29 +0530

    04-03-2025

--- src/career/Section5.jsx
+++ src/career/Section5.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Section5 = () => {
   useEffect(() => {
@@ -15,10 +17,10 @@ const Section5 = () => {
         Be the reason someone smiles today. Join Venkataeswara Hospital and embark on a fulfilling healthcare career!
         </p>
         <p className="career-sec-5-description">
-        Send your resume to: <a href="mailto:hrd@vhospitals.com">hrd@vhospitals.com</a>
+        Send your resume to: <a href="mailto:hrd@vhospitals.com"> <MdEmail/> hrd@vhospitals.com</a>
         </p>
         <p className="career-sec-5-description">
-        For inquiries, contact: 8056137662 / 9941121555
+        For inquiries, contact:   <FaPhoneAlt /> 8056137662 <FaPhoneAlt /> 9941121555
         </p>
       </div>
     </div>
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -92,7 +92,6 @@ const Navbar = () => {
               </motion.ul>
             )}
           </li>
-          <li><Link to="/career">Career</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
